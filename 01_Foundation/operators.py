165272dde1a6c596c76171fce94356c6b62a2948
Md Zaid Siddique <87862008+Mdzaidsiddique@users.noreply.github.com>
2026-07-02 13:37:30 -0400

    Enhance documentation for bitwise operators
    Added explanations and examples for bitwise operators in Python.

--- 01_Foundation/operators.py
+++ 01_Foundation/operators.py
@@ -59,6 +59,25 @@ print(x is not y) # returns true if both are not the same object
 print("z" in "zaid")
 
 # 7- Bitwise operator: used to compare binary numbers
-# (&, |, ^, ~, <<, >>)
+a = 12 #1100
+b = 10 #1010
+
+# & (AND Operator) : Each output bit is 1 only if both input bits are 1
+print(a & b) # 1000 -> 1*2^3 + 0*2^2 + 0*2^1 + 0*2^1 = 8
+
+# | (OR Operator) : Each output bit is 1 if either input bit is 1
+print(a | b) # 1110 -> 1*2^3 + 1*2^2 + 1*2^1 + 0*2^1 = 14
+
+# ^ (XOR Operator) : Each output bit is 1 if the input bits are different
+print(a ^ b) # 0110 -> 0*2^3 + 1*2^2 + 1*2^1 + 0*2^1 = 6
+
+# ~ (NOT Operator) : Flips every bit which will be equivalent to -(a+1)
+print(~a) # a~ = -(a+1) = -(20+1) = -21
+
+# << (Left shift) : say n = 2 : shift left by 2 places: move all bits two slots to the left, adding two 0s at the end.
+print(a << n) # ...001100 -> ..110000 = 48 (mathmatically: a*2^n = 12*2^2 = 12*4 = 48) 
+
+# >> (Right shift) : say n = 2 : shift right by 2 places: move all bits two slots to the right, removing two 0s at the end.
+print(a >> n) # ...001100 -> ..0011 = 3 (mathmatically: a/(2^n) = 12/(2^2) = 12/4 = 3
 
 # reference: https://www.w3schools.com/python/trypython.asp?filename=demo_oper_and
